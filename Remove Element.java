72aeb8fdda7ebdcec00b302a289dc5823bd0b153
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-06-30 23:20:55 +0530

    Add files via upload

---
+++ Remove Element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int removeElement(int[] nums, int val) 
+    {
+        int indx=0;
+    for(int i=0;i<nums.length;i++)
+        if(nums[i]!=val) nums[indx++]=nums[i];    
+    
+    return indx;
+        
+    }
+}
