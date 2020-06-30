dc694bf5b5c10feb869efc74ac6c8dff2446e403
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-06-30 23:20:48 +0530

    Add files via upload

---
+++ Number of Steps to Reduce a Number to Zero.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numberOfSteps (int num) 
+    {
+        int count=0;
+        while(num>0)
+        {
+            if((num&1)==0)
+            {
+                count++;
+                num=num>>1;
+            }
+            else
+            {
+                num--;
+                count++;
+            }
+        }
+        return count;
+        
+        
+    }
+}
