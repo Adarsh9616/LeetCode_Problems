fb78aaa0a4a9b777e9846ae0cc69345c6bb09ddf
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-07-07 23:06:12 +0530

    Add files via upload

---
+++ Number of Recent Calls.java
@@ -0,0 +1,23 @@
+class RecentCounter 
+{
+    Queue<Integer> q=new LinkedList<>();;
+    public RecentCounter() 
+    {
+    }
+    
+    public int ping(int t) 
+    {
+        q.offer(t);
+        while(q.peek()<(t-3000))
+        {
+            q.poll();
+        }
+        return q.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
