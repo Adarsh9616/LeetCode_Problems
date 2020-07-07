428a4e6b8bbb53dc10802e858362d3982bdf8ede
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-07-07 23:08:22 +0530

    Add files via upload

---
+++ Fizz Buzz.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<String> fizzBuzz(int n) 
+    {
+        List<String> ans=new ArrayList<>();
+        for(int i=1;i<=n;i++)
+        {
+            if(i%15==0)ans.add("FizzBuzz");
+            else if(i%5==0)ans.add("Buzz");
+            else if(i%3==0)ans.add("Fizz");
+            else ans.add(String.valueOf(i));
+        }
+        return ans;
+        
+    }
+}
