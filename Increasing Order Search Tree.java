027273832612359afcd3882635ff406be701226b
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-07-07 23:07:18 +0530

    Add files via upload

---
+++ Increasing Order Search Tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution 
+{
+     TreeNode cur;
+    public TreeNode increasingBST(TreeNode root) 
+    {
+        TreeNode ans = new TreeNode(0);
+        cur = ans;
+        inorder(root);
+        return ans.right;
+    }
+    public void inorder(TreeNode node) 
+    {
+        if (node == null) return;
+        inorder(node.left);
+        node.left = null;
+        cur.right = node;
+        cur = node;
+        inorder(node.right);
+    }
+}
