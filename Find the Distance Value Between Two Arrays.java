417d809492e5869648a9754b075e138417dac87c
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-07-11 02:38:01 +0530

    Add files via upload

---
+++ Find the Distance Value Between Two Arrays.java
@@ -0,0 +1,25 @@
+class Solution 
+{
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) 
+    {
+        boolean isElementMetDistanceCriteria = true;
+        int count = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            isElementMetDistanceCriteria = true;
+            for (int j = 0; j < arr2.length; j++) {
+                if (Math.abs(arr1[i] - arr2[j]) <= d) {
+                    isElementMetDistanceCriteria = false;
+                    break;
+                }
+
+            }
+
+            if (isElementMetDistanceCriteria){
+                count++;
+            }
+
+
+        }
+        return count;
+    }
+}
