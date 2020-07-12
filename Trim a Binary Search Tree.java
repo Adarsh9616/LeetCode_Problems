93b346dc2d676b5c09d36fb8e17ba1b73269ddc3
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-07-12 23:55:38 +0530

    Add files via upload

---
+++ Trim a Binary Search Tree.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution 
+{    
+    public TreeNode trimBST(TreeNode root, int L, int R) 
+    {
+        if(root==null) return root;
+        if(root.val>R) return trimBST(root.left,L,R);
+        if(root.val<L) return trimBST(root.right,L,R);
+        root.left=trimBST(root.left,L,R);
+        root.right=trimBST(root.right,L,R);
+        return root;
+    } 
+}
