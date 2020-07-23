fd0ce1732679577cb39b4fb9e468ebfeb5d87664
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-07-23 23:02:40 +0530

    Add files via upload

---
+++ Employee Importance.java
@@ -0,0 +1,25 @@
+/*
+// Definition for Employee.
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
+*/
+
+class Solution
+{
+   Map<Integer, Employee> emap;
+    public int getImportance(List<Employee> employees, int queryid) {
+        emap = new HashMap();
+        for (Employee e: employees) emap.put(e.id, e);
+        return dfs(queryid);
+    }
+    public int dfs(int eid) {
+        Employee employee = emap.get(eid);
+        int ans = employee.importance;
+        for (Integer subid: employee.subordinates)
+            ans += dfs(subid);
+        return ans;
+    }
+}
