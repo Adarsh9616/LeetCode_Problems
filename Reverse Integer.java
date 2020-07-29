b9763653866d8a3eb735f7c888c0058ebc90b6dd
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-07-29 22:23:55 +0530

    Add files via upload

---
+++ Reverse Integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverse(int x) 
+    {
+String ans = x < 0 ? new StringBuilder(String.valueOf(-x)).append("-").reverse().toString()
+                : new StringBuilder(String.valueOf(x)).reverse().toString();
+        try {
+            return Integer.parseInt(ans);
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+}
