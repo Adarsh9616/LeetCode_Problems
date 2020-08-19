d3e48e8ca232c969867aaa34d2acabdc167766d1
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-08-19 22:56:12 +0530

    Add files via upload

---
+++ 1-bit and 2-bit Characters.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) 
+    {
+        int i = 0;
+        while (i<bits.length-1)
+        {
+            if (bits[i]==1)
+            {
+                i+=2;
+            }
+            else
+            {
+                i++;
+            }
+        }
+         return i==bits.length-1;
+        
+    }
+}
