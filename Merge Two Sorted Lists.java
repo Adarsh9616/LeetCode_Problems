3b3df3e14d6f3cfe69f07c76d211c5ddf2547f70
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-08-23 22:09:30 +0530

    Add files via upload

--- Merge Two Sorted Lists.java
+++ Merge Two Sorted Lists.java
@@ -8,25 +8,28 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class Solution 
+{
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) 
     {
-       if(l1 == null){
-            return l2;
-        }
-        if(l2 == null){
+       if(l1==null)
+       {
+           return l2;
+       }
+        if(l2==null)
+        {
             return l1;
         }
-        
-        ListNode mergeHead;
-        if(l1.val < l2.val){
-            mergeHead = l1;
-            mergeHead.next = mergeTwoLists(l1.next, l2);
+        if(l1.val<l2.val)
+        {
+            l1.next=mergeTwoLists(l1.next,l2);
+            return l1;
         }
-        else{
-            mergeHead = l2;
-            mergeHead.next = mergeTwoLists(l1, l2.next);
+        else
+        {
+            l2.next=mergeTwoLists(l1,l2.next);
+            return l2;
         }
-        return mergeHead;
+            
     }
 }
