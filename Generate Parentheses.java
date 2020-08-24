874be1583b1b537240d4a12048670b20162c54f7
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-08-24 23:23:16 +0530

    Add files via upload

---
+++ Generate Parentheses.java
@@ -0,0 +1,25 @@
+class Solution 
+{
+    public List<String> generateParenthesis(int n) 
+    {
+        List<String> ans=new ArrayList<>();
+        backtrack(ans,"",0,0,n);
+        return ans;
+    }
+    public void backtrack(List<String> ans,String cur,int open,int close,int max)
+    {
+        if(cur.length()==2*max)
+        {
+            ans.add(cur);
+            return;
+        }
+        if(open<max)
+        {
+            backtrack(ans,cur+"(",open+1,close,max);
+        }
+        if(close<open)
+        {
+            backtrack(ans,cur+")",open,close+1,max);
+        }
+    }
+}
