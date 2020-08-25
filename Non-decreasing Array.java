ee4853495a7709cff8d069751d5e7633f32251ef
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-08-25 23:13:17 +0530

    Add files via upload

---
+++ Non-decreasing Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) 
+    {
+       int cnt = 0;                                                           
+        for(int i = 1; i < nums.length && cnt<=1 ; i++){
+            if(nums[i-1] > nums[i]){
+                cnt++;
+                if(i-2<0 || nums[i-2] <= nums[i])nums[i-1] = nums[i]; 
+                else nums[i] = nums[i-1];                                
+            }
+        }
+        return cnt<=1; 
+    }
+}
