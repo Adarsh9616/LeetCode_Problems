b348fb378a813712cd4b7678f7e7176f2779a142
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-09-03 21:29:39 +0530

    Add files via upload

--- Contains Duplicate III.java
+++ Contains Duplicate III.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) 
     {
-        for(int i=0;i<nums.length-1;i++)
+        if(nums.length>9999)
         {
-            for(int j=i+1;j<nums.length;j++)
+            return false;
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<Math.min(i+k+1,nums.length);j++)
             {
-                if(Math.abs((long)nums[i]-(long)nums[j])<=t&&Math.abs(i-j)<=k)
+                if(Math.abs((long)nums[i]-(long)nums[j])<=t)
                 {
                     return true;
                 }
