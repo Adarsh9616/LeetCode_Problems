f5426c823ff7158ea27d97d16623e1422c934843
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-09-15 03:44:19 +0530

    Add files via upload

---
+++ Minimum Path Sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minPathSum(int[][] grid)
+    {
+        if(grid.length==0)
+        {
+            return 0;
+        }
+        for(int i=1;i<grid.length;i++)
+        {
+            grid[i][0]=grid[i-1][0]+grid[i][0];
+        }
+        for(int j=1;j<grid[0].length;j++)
+        {
+            grid[0][j]=grid[0][j-1]+grid[0][j];
+        }
+        for(int i=1;i<grid.length;i++)
+        {
+            for(int j=1;j<grid[0].length;j++)
+            {
+                grid[i][j]=Math.min(grid[i-1][j]+grid[i][j],grid[i][j-1]+grid[i][j]);
+            }
+        }
+        return grid[grid.length-1][grid[0].length-1];
+    }
+}
