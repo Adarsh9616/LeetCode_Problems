ef17e6371962e995612f51e14355714bd8f9c2ae
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-10-01 03:23:13 +0530

    Add files via upload

---
+++ First Missing Positive.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) 
+    {
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>0&&nums[i]<=n&&nums[i]!=i+1)
+            {
+                int temp=nums[i]-1;
+                nums[i]=nums[temp];
+                nums[temp]=temp+1;
+                if(nums[i]!=temp+1)
+                    i--;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=i+1)
+            {
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+}
