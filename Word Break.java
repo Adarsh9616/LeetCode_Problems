f93bd6a82246f81f7cc1a17ba8a5fd944d1b0222
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-09-30 21:58:30 +0530

    Add files via upload

--- Word Break.java
+++ Word Break.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) 
-    {
+    {    
         boolean dp[]=new boolean[s.length()+1];
         dp[0]=true;
-        for(int i=1;i<s.length()+1;i++)
+        for(int i=1;i<=s.length();i++)
         {
-            boolean check=false;
             String str=s.substring(0,i);
+            boolean check=false;
             for(String a:wordDict)
             {
                 if(str.endsWith(a))
