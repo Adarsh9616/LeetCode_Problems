eb039f9432b8fe5f45432406ed175f10d5c58936
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-10-02 01:24:00 +0530

    Add files via upload

--- Maximum XOR of Two Numbers in an Array.java
+++ Maximum XOR of Two Numbers in an Array.java
@@ -1,14 +1,80 @@
-class Solution {
-    public int findMaximumXOR(int[] nums) 
+class Solution 
+{
+    class TrieNode
     {
-        int ans=0;
-        for(int i=0;i<nums.length-1;i++)
+        TrieNode left0;
+        TrieNode right1;
+    }
+    public void insert(TrieNode head,int val)
+    {
+        TrieNode curr=head;
+        for(int i=31;i>=0;i--)
         {
-            for(int j=i+1;j<nums.length;j++)
+            int bit=(val>>i)&1;
+            if(bit==0)
             {
-                ans=Math.max(ans,nums[i]^nums[j]);
+                if(curr.left0==null)
+                {
+                    curr.left0=new TrieNode();
+                }
+                curr=curr.left0;
+            }
+            else
+            {
+                if(curr.right1==null)
+                {
+                    curr.right1=new TrieNode();
+                }
+                curr=curr.right1;
             }
         }
-        return ans;
+    }
+    public int maxValue(TrieNode head,int val)
+    {
+        int temp=0;
+        TrieNode curr=head;
+        for(int i=31;i>=0;i--)
+        {
+            int bit=(val>>i)&1;
+            if(bit==0)
+            {
+                if(curr.right1!=null)
+                {
+                    temp+=(int)Math.pow(2,i);
+                    curr=curr.right1;
+                }
+                else
+                {
+                    curr=curr.left0;
+                }
+            }
+            else
+            {
+                if(curr.left0!=null)
+                {
+                    temp+=(int)Math.pow(2,i);
+                    curr=curr.left0;
+                }
+                else
+                {
+                    curr=curr.right1;
+                }
+            }
+        }
+        return temp;
+    }
+    public int findMaximumXOR(int[] nums) 
+    {        
+        TrieNode head=new TrieNode();
+        for(int i:nums)
+        {
+            insert(head,i);
+        }
+        int max=Integer.MIN_VALUE;
+        for(int i:nums)
+        {
+            max=Math.max(max,maxValue(head,i));
+        }
+        return max;
     }
 }
