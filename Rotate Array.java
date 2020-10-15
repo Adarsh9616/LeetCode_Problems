cbef9aaa7f3229c6437eb818c2cf1a7a438e756f
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-10-16 02:32:30 +0530

    Add files via upload

--- Rotate Array.java
+++ Rotate Array.java
@@ -1,20 +1,18 @@
-class Solution 
-{
-   public void rotate(int[] nums, int k)
-   {
-    k %= nums.length;
-    reverse(nums, 0, nums.length - 1);
-    reverse(nums, 0, k - 1);
-    reverse(nums, k, nums.length - 1);
-  }
-  public void reverse(int[] nums, int start, int end) 
-  {
-    while (start < end) {
-      int temp = nums[start];
-      nums[start] = nums[end];
-      nums[end] = temp;
-      start++;
-      end--;
+class Solution {
+    public void rotate(int[] nums, int k) 
+    {
+        k=k%nums.length;
+        reverse(nums,nums.length-k);
+        reverse(nums,nums.length);
+        reverse(nums,k);
     }
-  }
+    public void reverse(int nums[],int k)
+    {
+        for(int i=0;i<k/2;i++)
+        {
+            int temp=nums[i];
+            nums[i]=nums[k-i-1];
+            nums[k-i-1]=temp;
+        }
+    }
 }
