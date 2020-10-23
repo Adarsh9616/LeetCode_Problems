16279dd0a3910fa809fb46d85c25c8fc2e7d0fe9
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-10-23 23:48:59 +0530

    Add files via upload

---
+++ Clone Graph.java
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) 
+    {
+         if (node == null) return null;
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(node);
+        Map<Node, Node> map = new HashMap<Node, Node>();
+        map.put(node, new Node(node.val));
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            for (Node neighbor : current.neighbors) {
+                if (!map.containsKey(neighbor)) {
+                    map.put(neighbor, new Node(neighbor.val));
+                    queue.add(neighbor);
+                }
+                map.get(current).neighbors.add(map.get(neighbor));
+            }
+        }
+
+        return map.get(node);
+    }
+}
